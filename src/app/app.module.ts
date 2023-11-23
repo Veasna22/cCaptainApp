17f57d7c56a6d88acb28ecd977f6e62f02dc49d3
Ea Veasna <eaveasna52@gmail.com>
2023-11-23 21:06:33 +0700

    feat(UI): add UI profile information

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import { RegisterComponent } from './component/register/register.component';
 import { VerifyComponent } from './component/verify/verify.component';
 import { ResetpasswordComponent } from './component/resetpassword/resetpassword.component';
 import { FormsModule } from '@angular/forms';
-import { CustomerComponent } from './component/customer/customer.component';
 import { ProfileComponent } from './component/profile/profile.component';
 import { HomeComponent } from './component/home/home.component';
 import { CustomersComponent } from './component/customers/customers.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { StatsComponent } from './component/stats/stats.component';
 
 @NgModule({
   declarations: [
@@ -21,10 +22,11 @@ import { CustomersComponent } from './component/customers/customers.component';
     RegisterComponent,
     VerifyComponent,
     ResetpasswordComponent,
-    CustomerComponent,
     ProfileComponent,
     HomeComponent,
-    CustomersComponent
+    CustomersComponent,
+    NavbarComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,
