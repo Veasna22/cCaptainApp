01a57a0c8f755be746c502f679a4339d3d69d63c
Ea Veasna <eaveasna52@gmail.com>
2023-12-10 20:55:02 +0700

    feat(password) : add UpdatePassword

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -45,6 +45,14 @@ export class UserService {
           `${this.server}/user/update`, user)
         .pipe(tap(console.log), catchError(this.handleError))
     );
+    updatePassword$ = (form: { currentPassword: string, newPassword: string, confirmNewPassword: string }) => <Observable<CustomHttpResponse<Profile>>>
+    this.http.patch<CustomHttpResponse<Profile>>
+      (`${this.server}/user/update/password`, form)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+      
     refreshToken$ = () =>
       <Observable<CustomHttpResponse<Profile>>>(
         this.http
